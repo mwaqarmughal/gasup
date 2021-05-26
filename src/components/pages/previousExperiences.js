fc2bad331851a07b250adda5cf2f8cfbc80405ae
Muhammad Waqar Mughal <56934353+mwaqarmughal@users.noreply.github.com>
2021-05-26 22:16:39 +0500

    geoLocation

--- src/components/pages/previousExperiences.js
+++ src/components/pages/previousExperiences.js
@@ -1,19 +1,27 @@
 import React,{useEffect} from 'react'
 import '../../App.css'
 import Footer from  '../footer'
+import useGeoLocation from '../useGeoLocation';
 import './previousExperiences.css'
 
 
 
 function PreviousExperiences() {
+  const location = useGeoLocation()
+  ;
      useEffect(() => {
        window.scrollTo(0, 0);
      }, []);
     return (
       <div className="">
+        <div className="inline-block mr-auto pt-1">
+          {location.loaded
+            ? JSON.stringify(location)
+            : "Location data not available yet."}
+        </div>
         <div className="container hei">
-            <h1 className='mt-5 text-center text-dark'>Previous Experiences</h1>
-            </div>
+          <h1 className="mt-5 text-center text-dark">Previous Experiences</h1>
+        </div>
         <Footer />
       </div>
     );
--- src/components/pages/serviceArea.js
+++ src/components/pages/serviceArea.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import Footer from '../footer';
 import '../../App.css'
 import {Map, InfoWindow, Marker,GoogleApiWrapper} from 'google-maps-react'
+import useGeoLocation from '../useGeoLocation';
 
 
 class serviceArea extends Component{
@@ -10,7 +11,12 @@ class serviceArea extends Component{
     <div>
       <div className="text-center background-height">
         <div>
-        <h1 className='bg-transparent'>Service Areas</h1>
+          <h1 className="bg-transparent">Service Areas</h1>
+          <div className="inline-block mr-auto pt-1">
+            {location.loaded
+              ? JSON.stringify(location)
+              : "Location data not available yet."}
+          </div>
           <Map google={this.props.google} zoom={14}>
             <Marker onClick={this.onMarkerClick} name={"current location"} />
             <InfoWindow onClose={this.InfoWindowClose}></InfoWindow>
